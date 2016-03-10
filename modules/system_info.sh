9a127c643f4668597471f4fe9184b69fc1a06e91
L1ghtn1ng <townsend891@hotmail.com>
2016-03-10 06:46:45 +0000

    Added more hardware detection

--- modules/system_info.sh
+++ modules/system_info.sh
@@ -4,18 +4,21 @@
 # Please give me credit thats all I ask
 # Thank you
 #
-#These are the variables I have set Do Not Change these unless you know what you are doing 
-CpuName=`cat /proc/cpuinfo | sort | uniq -c | grep 'name' | cut -d ':' -f2`
-Cpucores=`cat /proc/cpuinfo | sort | uniq -c | grep 'cores' | cut -d ':' -f2`
+#These are the variables I have set Do Not Change these unless you know what you are doing
+motherboard=`sudo dmidecode -s system-manufacturer`
+Biosversion=`sudo dmidecode -s bios-version`
+motherboardmodel=`sudo dmidecode -s baseboard-product-name`
+CpuName=`lscpu | grep name | cut -d ':' -f2`
+Cpucores=`lscpu | grep 'socket' | cut -d ':' -f2`
 RamTotal=`cat /proc/meminfo | grep 'MemTotal' | cut -d ':' -f2`
 RamFree=`cat /proc/meminfo | grep 'MemFree' | cut -d ':' -f2`
 RamSpeed=`sudo dmidecode -t memory | sort | uniq -c | grep Speed | cut -d ':' -f2 | uniq`
 RamSupported=`sudo dmidecode -t memory | sort | uniq -c | grep 	Maximum" "Capacity | cut -d ':' -f2`
 Battery=`acpi -V | grep 'Battery' | cut -d ':' -f2`
 Cpuspeed=`sudo dmidecode | sort | uniq -c | grep Current" "Speed | cut -d ':' -f2`
-Cpumax=`sudo dmidecode | sort | uniq -c | grep Max" "Speed | cut -d ':' -f2`
+Cpumax=`lscpu | grep max | cut -d ':' -f2 | cut -d '.' -f1`
 Cputhreads=`sudo dmidecode | sort | uniq -c | grep Thread" "Count | cut -d ':' -f2`
-CpuCharacteristics=`sudo dmidecode | sort | grep 64-bit | cut -d ':' -f2`
+CpuCharacteristics=`lscpu | egrep 64-bit | cut -d ',' -f2`
 RamType=`sudo dmidecode | sort | uniq | grep DDR | cut -d ':' -f2`
 GPU=`cat /var/log/Xorg.0.log | egrep '(NVIDIA GPU|INTEL GPU|AMD GPU)' | uniq -u | cut -d '(' -f3 | cut -d ':' -f2 | awk '{print$3,$4,$5,$6}'`
 Kernelversion=`uname -r | cut -d 'g' -f1 | sed -e  "s/-*$//"`
@@ -26,7 +29,7 @@ cputemp=`acpi -t | cut -d ',' -f2`
 ScreenSize=`cat /var/log/Xorg.0.log | grep Virtual" "screen | sort | uniq -u | cut -d ':' -f2 | awk '{print$7,$8,$9}'`
 User=`whoami`
 LoginName=`logname`
-IP=`ifconfig | grep 'Bcast' | awk '{print$2}' | cut -d ':' -f2`
+IP=`ip a | egrep '(eth0|eno1)' | egrep inet | awk '{print$2}'`
 Public_ip=`wget -q "http://ipecho.net/plain" -O publicip; cat publicip; rm publicip`
 Ethernet_controller=`lspci -Q | grep 'Ethernet controller' | cut -d ':' -f3 | cut -d '(' -f1`
 Ethernetmac=`ifconfig | egrep '(eth0|eno1)' | awk '{print$5}'`
@@ -60,13 +63,16 @@ echo -e "\e[1;32m===================================================\e[0m"
 
 hardware_menu(){
 clear
+echo -e "\e[1;31mMotherboard Manufacturer:\e[0m" $motherboard
+echo -e "\e[1;31mBios Version:\e[0m" $Biosversion
+echo -e "\e[1;31mMotherboard Model:\e[0m" $motherboardmodel
 echo -e "\e[1;31mCpu Model:\e[0m" $CpuName
 echo -e "\e[1;31mCpu Cores:\e[0m" $Cpucores
 echo -e "\e[1;31mCpu Threads:\e[0m" $Cputhreads
-echo -e "\e[1;31mCpu Temperature:\e[0m" $cputemp
 echo -e "\e[1;31mCurrent Cpu Speed:\e[0m" $Cpuspeed
-echo -e "\e[1;31mMax Cpu Speed:\e[0m" $Cpumax
+echo -e "\e[1;31mMax Cpu Speed MHz:\e[0m" $Cpumax
 echo -e "\e[1;31mCpu Characteristics:\e[0m" $CpuCharacteristics
+echo -e "\e[1;31mCpu Temperature:\e[0m" $cputemp
 echo -e "\e[1;31mRam Total:\e[0m" $RamTotal  
 echo -e "\e[1;31mRam Free:\e[0m" $RamFree
 echo -e "\e[1;31mRam Type:\e[0m" $RamType
@@ -75,7 +81,7 @@ echo -e "\e[1;31mMax Ram Supported:\e[0m" $RamSupported
 echo -e "\e[1;31mScreen Resolution:\e[0m" $ScreenSize
 echo -e "\e[1;31mGraphics Card:\e[0m" $GPU
 echo -e "\e[1;31mEthernet Controller:\e[0m" $Ethernet_controller
-echo -e "\e[1;31mNetwork Controller:\e[0m" $Network_controller
+echo -e "\e[1;31mWireless Controller:\e[0m" $Network_controller
 echo -e "\e[1;31mKernel Version:\e[0m" $Kernelversion
 echo -e "\e[1;31mBattery:\e[0m" $Battery
 echo
